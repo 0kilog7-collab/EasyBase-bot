d63a424f95f14607ad96a534dd742f21b5d2a115
0kilog7-collab <0kilog7@gmail.com>
2026-06-29 16:15:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from flask import Flask, render_template_string
+import os
+
+app = Flask(__name__)
+
+HTML = '''
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -178,3 +184,12 @@
     </div>
 </body>
 </html>
+'''
+
+@app.route('/')
+def home():
+    return render_template_string(HTML)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
